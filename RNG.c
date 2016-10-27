3e4ca26eeb5cf03b8588f642c1f7023ebce3f324
Yueyang <yxu8@stuy.edu>
2016-10-27 03:12:26 +0100

    Adds file checking and polishes stuff

--- RNG.c
+++ RNG.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 int randomInt()
 {
@@ -20,19 +22,32 @@ int main()
 {
   int randArr[10];
   int i;
+  
   printf("original array\n");
   for(i=0;i<10;i++)
     {
       *(randArr+i)=randomInt();
       printf("random %d: %d \n",i,*(randArr+i));
     }
-  umask(0000);
+  
+  umask(0000);//setting mask
+  
   int fd;
   fd=open("results",O_CREAT,0666);
+  //checks for error
+  if ( fd == -1 ) {
+    printf( "error: %d - %s\n", errno, strerror(errno) );
+  }
+  
   printf("\n \n writing numbers into file\n");
   write(fd,randArr,10*sizeof(int));
   close(fd);
   fd = open("results",O_RDONLY);
+  //checks for error
+  if ( fd == -1 ) {
+    printf( "error: %d - %s\n", errno, strerror(errno) );
+  }
+  
   printf("\n \n reading numbers from file\n");
   read(fd,randArr,10*sizeof(10));
   printf("\nnew array\n");
@@ -41,6 +56,6 @@ int main()
       //*(randArr+i)=randomInt();
       printf("random %d: %d \n",i,*(randArr+i));
     }
-  
+  close(fd);
   return 0;
 }
